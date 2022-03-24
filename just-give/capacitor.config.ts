b6c5c563a495ccd9303a2ba443d234f829132daa
SafaeQ <safaedani009@gmail.com>
2022-03-24 11:40:32 +0100

    ✔ set up the project

---
+++ just-give/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Just give',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
