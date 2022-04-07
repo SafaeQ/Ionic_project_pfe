0d0339310a26ada3127f4511d515b68dc05def8d
SafaeQ <safaedani009@gmail.com>
2022-04-07 15:14:07 +0000

    🐱‍👤✔ ionic just-give

---
+++ server/models/article.model.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const articleSchema = new Schema(
+  {
+    title: { 
+        type: String,
+        require: true,
+        lowercase: true
+        },
+    description: {
+        type: Number,
+        default: 0 
+        },
+    image: { 
+        type: Array,
+        data: Buffer
+        },
+    project_id: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: 'Project'
+    }
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  }
+);
+
+const Article = mongoose.model("Article", articleSchema);
+
+module.exports = { Article };
