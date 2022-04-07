d7b3a2838a23afedcb3779c9c4aac530f0b03724
SafaeQ <safaedani009@gmail.com>
2022-04-07 15:28:52 +0000

    🐱‍👤✔ ionic just-give

--- server/routes/userAuth.routes.js
+++ server/routes/userAuth.routes.js
@@ -1,16 +1,16 @@
 const userAuth_router = require('express').Router()
 
-const { assoc_signup }= require('../controllers/auth/userAuth.controller')
+const {assoc_signup, login, donor_signup }= require('../controllers/auth/userAuth.controller')
 
 const {uploads} = require('../utils/uploadImage')
 
 
 
-    userAuth_router.post('/register',uploads ,assoc_signup)
+    userAuth_router.post('/register-association',uploads ,assoc_signup)
 
-    userAuth_router.post('/register', assoc_signup)
+    userAuth_router.post('/register-donor', donor_signup)
 
-    userAuth_router.post('/register', assoc_signup)
+    userAuth_router.post('/login', login)
 
 
 module.exports = userAuth_router
