d51a6a036fb82971556fb2bf7b13abfd228e3db7
SafaeQ <safaedani009@gmail.com>
2022-04-29 13:35:27 +0000

    🐱✔ ionic just-give

--- server/routes/project.routes.js
+++ server/routes/project.routes.js
@@ -5,7 +5,7 @@ const createProject = require('../controllers/project.controller')
 const upload = require('../utils/uploadImage')
 
 
-    projectRouter.post('/create-project', upload.array('image', 8), createProject)
+    projectRouter.post('/create-project', upload.array('images', 8), createProject)
 
 
 module.exports = projectRouter
