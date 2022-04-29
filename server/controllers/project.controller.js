02e82674ae3a7efbe52a5125036d566661ed2036
SafaeQ <safaedani009@gmail.com>
2022-04-29 14:04:10 +0000

    🐱✔ ionic just-give

--- server/controllers/project.controller.js
+++ server/controllers/project.controller.js
@@ -31,31 +31,19 @@ const getAllProject = (req, res) => {
     
     Project.find()
         .then(data => {
-            var message = "";
-            if (data === undefined || data.length == 0) message = "No product found!";
-            else message = 'Products successfully retrieved';
+            
+            let message = "";
 
-            res.send({
-                success: true,
-                message: message,
-                data: data
-            });
+            if (data === undefined || data.length == 0) message = "No project found!";
+
+            else message = 'projects successfully retrieved';
+
+            res.send({ success: true, message: message, data: data });
+
         }).catch(err => {
-        res.status(500).send({
-            success: false,
-            message: err.message || "Some error occurred while retrieving products."
-        });
+
+        res.status(500).send({ success: false, message: err.message || "Some error occurred while retrieving projects." });
     });
-
-    // try {
-    //     const project = await Project.find({})
-
-    //     (!project) ? res.status(400).send('some thing wrong') : res.status(200).send(project)
-
-    // } catch (error) {
-
-    //     res.status(500).send(error)
-    // }
 }
 
 module.exports = {createProject, getAllProject}
