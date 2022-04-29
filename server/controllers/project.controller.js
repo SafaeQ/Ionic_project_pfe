6436048d7e8fdd513de352fd2801669bc897beac
SafaeQ <safaedani009@gmail.com>
2022-04-29 13:57:59 +0000

    🐱✔ ionic just-give

--- server/controllers/project.controller.js
+++ server/controllers/project.controller.js
@@ -4,7 +4,7 @@ const Project = require('../models/project.model')
 const createProject = async (req, res) => {
 
     const {name, budget, category, description, association} = req.body
-    console.log('hello ', req.body);
+    
     try {
         const imageFiles = req.files
 
@@ -27,4 +27,35 @@ const createProject = async (req, res) => {
     }
 }
 
-module.exports = createProject
+const getAllProject = (req, res) => {
+    
+    Project.find()
+        .then(data => {
+            var message = "";
+            if (data === undefined || data.length == 0) message = "No product found!";
+            else message = 'Products successfully retrieved';
+
+            res.send({
+                success: true,
+                message: message,
+                data: data
+            });
+        }).catch(err => {
+        res.status(500).send({
+            success: false,
+            message: err.message || "Some error occurred while retrieving products."
+        });
+    });
+
+    // try {
+    //     const project = await Project.find({})
+
+    //     (!project) ? res.status(400).send('some thing wrong') : res.status(200).send(project)
+
+    // } catch (error) {
+
+    //     res.status(500).send(error)
+    // }
+}
+
+module.exports = {createProject, getAllProject}
--- server/routes/project.routes.js
+++ server/routes/project.routes.js
@@ -1,11 +1,13 @@
 const projectRouter = require('express').Router()
 
-const createProject = require('../controllers/project.controller')
+const {createProject, getAllProject} = require('../controllers/project.controller')
 
 const upload = require('../utils/uploadImage')
 
 
     projectRouter.post('/create-project', upload.array('images', 8), createProject)
+
+    projectRouter.get('/projects', getAllProject)
 
 
 module.exports = projectRouter
