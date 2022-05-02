e69b424256b2e404488ed0d9e95b3e9f2391a5d1
SafaeQ <safaedani009@gmail.com>
2022-05-02 15:46:38 +0000

    article's routes

---
+++ server/routes/article.routes.js
@@ -0,0 +1,19 @@
+const articleRouter = require('express').Router()
+
+const {createArticle, getAllArticles, getArticleById, updateArticle, deleteArticle} = require('../controllers/project.controller')
+
+const upload = require('../utils/uploadImage')
+
+
+    articleRouter.post('/create-article', upload.array('images', 8), createArticle)
+
+    articleRouter.get('/articles', getAllArticles)
+
+    articleRouter.get('/article/:id', getArticleById)
+
+    articleRouter.put('/update-article/:id', updateArticle)
+
+    articleRouter.put('/delete-article/:id', deleteArticle)
+
+
+module.exports = articleRouter
