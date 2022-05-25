7f495a7c8289442413eadd37f66fa17db90b8947
SafaeQ <safaedani009@gmail.com>
2022-05-25 14:52:41 +0100

    🐱‍👤 set up

--- server/utils/deleteFile.js
+++ server/utils/deleteFile.js
@@ -3,7 +3,7 @@ const fs = require('fs')
 const deletedImage = (unwantedImages)=>{
     for (const unwantedImage of unwantedImages) {
         fs.unlink(
-            `.../api/register/${unwantedImage}.filename`,
+            `../api/register/${unwantedImage}.filename`,
             (err)=>{
                 if(err) throw err
                 console.log('image deleted');
