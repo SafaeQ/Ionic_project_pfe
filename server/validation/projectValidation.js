e9f4c8c43bdb274d8eae7eaf3809609ea334e266
SafaeQ <safaedani009@gmail.com>
2022-05-25 14:44:23 +0100

    🐱‍👤 set up

--- server/validation/projectValidation.js
+++ server/validation/projectValidation.js
@@ -3,8 +3,8 @@ const Joi = require("joi");
 const projectValidation = (data) => {
   const schema = Joi.object({
     name: Joi.string().min(5).required(),
-    email: Joi.string().min(6).required().email(),
-    password: Joi.string().min(6).required(),
+    budget: Joi.number().min(6).required(),
+    description: Joi.string().min(20).required(),
 });
   return schema.validate(data);
 };
