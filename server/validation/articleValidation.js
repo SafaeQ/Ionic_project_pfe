57833e0a03da8027aab30462a09218ce2e9ac563
SafaeQ <safaedani009@gmail.com>
2022-05-29 18:55:49 +0100

    💄 add validation to controller

--- server/validation/articleValidation.js
+++ server/validation/articleValidation.js
@@ -1,11 +1,14 @@
 const Joi = require("joi");
 
+Joi.objectId = require('joi-objectid')(Joi)
+
 const articleValidation = (data) => {
   const schema = Joi.object({
     title: Joi.string().min(5).required(),
     date: Joi.date().min('01-01-2003').required(),
     description: Joi.string().min(20).required(),
     progress: Joi.boolean().required(),
+    project_id: Joi.objectId(),
 });
   return schema.validate(data);
 };
