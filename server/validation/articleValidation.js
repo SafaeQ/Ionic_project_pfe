b0d7b7cee6ad8d2a91ab4e2421b307d1fbaa5c54
SafaeQ <safaedani009@gmail.com>
2022-05-29 18:24:21 +0100

    💄 add validation

--- server/validation/articleValidation.js
+++ server/validation/articleValidation.js
@@ -2,9 +2,10 @@ const Joi = require("joi");
 
 const articleValidation = (data) => {
   const schema = Joi.object({
-    name: Joi.string().min(5).required(),
-    budget: Joi.number().min(6).required(),
+    title: Joi.string().min(5).required(),
+    date: Joi.date().min('01-01-2003').required(),
     description: Joi.string().min(20).required(),
+    progress: Joi.boolean().required(),
 });
   return schema.validate(data);
 };
