97dfd04bdaaa5ecd50076bcc47e9f10af5f1ddfe
SafaeQ <safaedani009@gmail.com>
2022-06-09 11:20:31 +0100

    ✅ finish signup donor page UI

---
+++ frontend/src/components/Auth-Association/SignupAssoc.tsx
@@ -0,0 +1,74 @@
+import { IonBackButton, IonButton, IonButtons, IonCardTitle, IonCol, IonContent, IonFooter,IonItem, IonLabel, IonInput, IonGrid, IonHeader, IonIcon, IonPage, IonRouterLink, IonRow, IonToolbar } from '@ionic/react';
+
+import { arrowBack, shapesOutline } from "ionicons/icons";
+import { Action } from '../utils/Action';
+import { Wave } from '../utils/Wave';
+
+const SignupAssoc = () => {
+
+	return (
+		<IonPage>
+			<IonHeader>
+				<IonToolbar>
+					
+                    <IonButtons slot="start">
+                        <IonBackButton icon={ arrowBack } text="" className="custom-back" />
+                    </IonButtons>
+                    <IonButtons slot="end">
+                        <IonButton className="custom-button">
+                            <IonIcon icon={ shapesOutline } />
+                        </IonButton>
+                    </IonButtons>
+				</IonToolbar>
+			</IonHeader>
+			<IonContent fullscreen>
+                <IonGrid className="ion-padding">
+                    <IonRow>
+                        <IonCol size="12" >
+                            <IonCardTitle>Sign up Donor</IonCardTitle>
+                        </IonCol>
+                    </IonRow>
+
+                    <IonRow className="ion-no-margin ">
+                        <IonCol >
+                            <IonItem>
+                                <IonLabel position='floating'> Full Name</IonLabel>
+                                <IonInput type='text'></IonInput>
+                            </IonItem>
+                        </IonCol>
+                    </IonRow>
+                    <IonRow className="ion-no-margin ">
+                        <IonCol >
+                            <IonItem>
+                                <IonLabel position='floating'> Your Email</IonLabel>
+                                <IonInput type='email'></IonInput>
+                            </IonItem>
+                        </IonCol>
+                    </IonRow>
+                    <IonRow className="ion-no-margin ">
+                        <IonCol>
+                            <IonItem>
+                                <IonLabel position='floating'> Your Password</IonLabel>
+                                <IonInput type='password'></IonInput>
+                            </IonItem>
+                        </IonCol>
+                    </IonRow>
+                    <IonRow className="ion-padding">
+                        <IonCol size="12" className="col-md-12 text-center">
+                            <IonButton color='danger' type='submit'> SignUp </IonButton>
+                        </IonCol>
+                    </IonRow>
+                </IonGrid>
+			</IonContent>
+
+			<IonFooter>
+				<IonGrid className="ion-no-margin ion-no-padding">
+                    <Action message="Already got an account?" text="Login" link="/login" />
+                    <Wave />
+				</IonGrid>
+			</IonFooter>
+		</IonPage>
+	);
+};
+
+export default SignupAssoc;
