e03cd67074df60c4c66c5a07103a88f3d8b25de4
SafaeQ <safaedani009@gmail.com>
2022-06-09 15:32:38 +0100

    ✅ finish signup donor page UI

--- frontend/src/components/Auth-Association/LoginAsso.tsx
+++ frontend/src/components/Auth-Association/LoginAsso.tsx
@@ -5,11 +5,37 @@ import { Wave } from '../utils/Wave';
 
 function LoginAsso() {
 
-    const initialState = {email: '', password: ''};
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [message, setMessage] = useState('');
+    const [showPassword, setShowPassword] = useState(false);
 
-    const [state, setstate] = useState(initialState);
-
+    const validateEmail = (email: string) => {
+        var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return re.test(email);
+    };
     
+    const validatePassword = (password: string) => {
+        let re = /[0-9]+/;
+        return re.test(password);
+    };
+
+
+    const handleSubmit = () => {
+        if (email === "" || password === "") {
+            setMessage("Fill in all fields");
+        } else if (!validateEmail(email)) {
+            setMessage("Only valid email addresses are accepted");
+        } else if (password.length <= 10) {
+            setMessage("Password should have more than 10 characters");
+        } else if (!validatePassword(password)) {
+            setMessage("Password should include numbers");
+        } else {
+            setMessage("");
+            setPassword("");
+            setEmail("");
+        }
+    };
 
 return (
     <IonPage>
@@ -30,7 +56,7 @@ return (
                     <IonCol >
                         <IonItem>
                             <IonLabel position='floating'> Your Email</IonLabel>
-                            <IonInput type='email'></IonInput>
+                            <IonInput name="email" type='email'></IonInput>
                         </IonItem>
                     </IonCol>
                 </IonRow>
@@ -38,13 +64,13 @@ return (
                     <IonCol>
                         <IonItem>
                             <IonLabel position='floating'> Your Password</IonLabel>
-                            <IonInput type='password'></IonInput>
+                            <IonInput name="passowrd" type='password'></IonInput>
                         </IonItem>
                     </IonCol>
                 </IonRow>
                 <IonRow className="justify-content-md-center">
                     <IonCol size="12" className="col-md-12 text-center">
-                        <IonButton color='danger' type='submit'> Login </IonButton>
+                        <IonButton color='danger' type='submit' onClick={handleSubmit}> Login </IonButton>
                     </IonCol>
                 </IonRow>
             </IonGrid>
