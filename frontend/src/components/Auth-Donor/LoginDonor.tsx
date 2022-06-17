9b3214e25ee2847cf75876f16d1a7563f8a038a1
SafaeQ <safaedani009@gmail.com>
2022-06-17 12:01:57 +0100

    ✔ fix login donor

--- frontend/src/components/Auth-Donor/LoginDonor.tsx
+++ frontend/src/components/Auth-Donor/LoginDonor.tsx
@@ -20,7 +20,7 @@ function LoginDonor() {
         var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
         return re.test(email);
     };
-    
+
     const validatePassword = (password: string) => {
         let re = /[0-9]+/;
         return re.test(password);
