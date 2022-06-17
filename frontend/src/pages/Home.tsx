afb53168d81a30528bf7360da60cde5fd51b3807
SafaeQ <safaedani009@gmail.com>
2022-06-17 14:45:42 +0100

    🐛 ✔ logic signup assoc

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-
 import { IonButton, IonCardTitle,IonToolbar,IonButtons, IonBackButton,IonIcon, IonCol, IonContent, IonFooter, IonGrid, IonHeader, IonImg, IonPage, IonRouterLink, IonRow, } from '@ionic/react';
 import { arrowBack, shapesOutline } from "ionicons/icons";
 
@@ -19,17 +18,13 @@ return (
                 </IonButtons>
 			</IonToolbar>
 		</IonHeader>
-        
 		<IonContent fullscreen>
-
-				
 					<IonGrid>
 						<IonRow >
 							<IonCol size="11">
 								<IonCardTitle>Join millions of other people discovering their creative side</IonCardTitle>
 							</IonCol>
 						</IonRow>
-
 						<IonRow className={ `ion-text-center ion-justify-content-center` }>
 							<IonRouterLink routerLink="/signup" className="custom-link">
 								<IonCol size="11">
@@ -37,10 +32,8 @@ return (
 								</IonCol>
 							</IonRouterLink>
 						</IonRow>
-					</IonGrid>
-			
+					</IonGrid>		
 		</IonContent>
-
 		<IonFooter>
 			<IonGrid>
 				{/* <Action message="Already got an account?" text="Login" link="/login" /> */}				</IonGrid>
