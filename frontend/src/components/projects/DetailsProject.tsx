5425cce691ac5562d407bc1c45ebba5fa589a055
SafaeQ <safaedani009@gmail.com>
2022-06-22 12:08:46 +0100

    ⚡️ ionic react just!give ➕🐱‍👤

--- frontend/src/components/projects/DetailsProject.tsx
+++ frontend/src/components/projects/DetailsProject.tsx
@@ -1,3 +1,5 @@
+import { IonButton, IonButtons, IonContent, IonHeader, IonIcon, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { shapesOutline } from 'ionicons/icons';
 import React, { useEffect, useState } from 'react';
 import api from '../../services/api';
 
@@ -18,9 +20,29 @@ const DetailsProject: React.FC = (props) => {
   }
 
   return (
-    <div>
-        {/* <h1> {projects.name} </h1> */}
-    </div>
+    <>
+      <IonPage>
+        <IonHeader>
+          <IonToolbar>
+            <IonButtons slot="end">
+                <IonButton className="custom-button">
+                        <IonIcon icon={ shapesOutline } />
+                </IonButton>
+            </IonButtons>
+          </IonToolbar>
+        </IonHeader>
+        <IonContent fullscreen> 
+            <IonHeader collapse="condense" >
+                <IonToolbar>
+                        <IonTitle style={{fontSize: 'larger'}}>details</IonTitle>  
+                </IonToolbar>
+            </IonHeader>
+        </IonContent>
+        <div>
+            {/* <h1> {projects.name} </h1> */}
+        </div>
+      </IonPage>
+    </>
   );
 }
 
