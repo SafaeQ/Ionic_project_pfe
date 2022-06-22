4f3de3ca79640c3fa11cb0c292e6cc508ec31c99
SafaeQ <safaedani009@gmail.com>
2022-06-22 11:52:31 +0100

    ⚡️ ionic react just!give ➕🐱‍👤

--- frontend/src/components/projects/DetailsProject.tsx
+++ frontend/src/components/projects/DetailsProject.tsx
@@ -6,22 +6,23 @@ import api from '../../services/api';
 const DetailsProject: React.FC = (props) => {
   const [projects, setProjects] = useState([]);
 
-  const fetchDatabyId = (id: string) => {
+  const fetchDatabyId = (id: string) => {   
     api.get(`/api/projects/${id}`)
 		.then((res) => {
-			setProjects(res.data.data)
+			setProjects(res.data)
+      console.log(res.data);
 		})
 		.catch(err => {
 			console.log(err);
 		})
   }
-  useEffect(()=>{
-    fetchDatabyId
-},[])
+//   useEffect(()=>{
+//     fetchDatabyId
+// },[])
 
   return (
     <div>
-        <h1> {projects.id} </h1>
+        {/* <h1> {projects.id} </h1> */}
     </div>
   );
 }
