a075eb007ab33834f82b9e28143d22bc0e87c4c6
SafaeQ <safaedani009@gmail.com>
2022-06-26 18:12:09 +0100

    ⚡️ ionic react just!give ➕🐱‍👤

--- frontend/src/components/article/AddArticle.tsx
+++ frontend/src/components/article/AddArticle.tsx
@@ -1,56 +1,11 @@
-// import React from 'react';
+import React from 'react';
 
-// function UpdateMeal({
-//     CloseUpdatePopup,
-//     showUpdate,
-//     updatedMeal,
-//     Restaurants,
-//     Categorys}) {
-//     const [newMeal, setnewMeal] = useState({});
-//     const handelChange = (e) => {
-//       setnewMeal({ ...newMeal, [e.target.name]: e.target.value });
-//     };
-//     useEffect(() => {
-//       setnewMeal(updatedMeal);
-//     }, [showUpdate, CloseUpdatePopup]);
-//     const handelSubmit = () => {
-//       try {
-//         API.patch(`meal/${updatedMeal._id}`, newMeal).then(
-//           () => {
-//             CloseUpdatePopup();
-//           }
-//         );
-//       } catch (error) {
-//         if (error.response) {
-//           console.log(error.response.data.message);
-//         }
-//         console.log(error.message);
-//       }
-//     };
+const AddArticle = () => {
+  return (
+    <div>
+        <h1> heyy </h1>
+    </div>
+  );
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const AddArticle = () => {
-//   return (
-//     <div>
-//         <h1> heyy </h1>
-//     </div>
-//   );
-// }
-
-// export default AddArticle;
+export default AddArticle;
